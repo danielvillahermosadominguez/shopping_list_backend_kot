378b8759c17d22e7af650a2dfae5841455463b92
Daniel Villahermosa <daniel.villahermosa@codurance.com>
2023-05-07 18:23:27 +0200

    spring boot project

---
+++ src/main/kotlin/com/shoppinglist/api/ShoppingListBackendKotApplication.kt
@@ -0,0 +1,11 @@
+package com.shoppinglist.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShoppingListBackendKotApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShoppingListBackendKotApplication>(*args)
+}
